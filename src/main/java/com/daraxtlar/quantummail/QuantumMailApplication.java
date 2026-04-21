e29822c8c5ddcf017403179b649cbe8743c55561
Daraxtlar <an.primary@protonmail.com>
2026-04-21 16:51:47 +0200

    initial commit

---
+++ src/main/java/com/daraxtlar/quantummail/QuantumMailApplication.java
@@ -0,0 +1,13 @@
+package com.daraxtlar.quantummail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuantumMailApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuantumMailApplication.class, args);
+    }
+
+}
