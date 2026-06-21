4faf02d25b5ff5d925a0910a82bbadb5f3d9eb94
Daraxtlar <an.primary@protonmail.com>
2026-06-21 12:46:06 +0200

    moved secret into application-local. added run configurations

--- src/main/java/com/daraxtlar/quantummail/service/JwtService.java
+++ src/main/java/com/daraxtlar/quantummail/service/JwtService.java
@@ -3,6 +3,7 @@ package com.daraxtlar.quantummail.service;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import java.nio.charset.StandardCharsets;
@@ -17,7 +18,9 @@ import java.util.Date;
  */
 @Service
 public class JwtService {
-    private final String secret = "mysecretkey_projekt_quantummail_java_project_piu";
+
+    @Value("${app.jwt.secret}")
+    private String secret;
 
     /**
      * Creates the cryptographic signing key used for JWT operations.
